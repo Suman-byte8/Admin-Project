00c66e9eea23dee7fb0a93b16dc55fcd2e63da72
SumanSaha <sumansahaweb.dev@gmail.com>
2025-11-05 20:54:56 +0530

    fix the column issue in home page description

--- src/components/PageManagement/HomePageBanner.jsx
+++ src/components/PageManagement/HomePageBanner.jsx
@@ -178,7 +178,7 @@ const HomePageBanner = () => {
           </div>
 
           {/* Title and Description */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="grid grid-cols-1  gap-4">
             <input
               type="text"
               placeholder="Title"
