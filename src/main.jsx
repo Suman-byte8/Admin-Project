4e5a0504351ad54efae91fb445811f106a130037
SumanSaha <sumansahaweb.dev@gmail.com>
2025-11-05 20:30:36 +0530

    Disable console logs when VITE_ENVIRONMENT is not development

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,22 @@ import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext'
 
+// Override console methods if not in development
+if (import.meta.env.VITE_ENVIRONMENT !== 'development') {
+  const noop = () => {};
+  console.log = noop;
+  console.warn = noop;
+  console.error = noop;
+  console.info = noop;
+  console.debug = noop;
+  console.table = noop;
+  console.group = noop;
+  console.groupEnd = noop;
+  console.time = noop;
+  console.timeEnd = noop;
+  console.trace = noop;
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
